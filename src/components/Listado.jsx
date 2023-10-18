594141c35c1f8aa46aa28a327e9ec87da8c2a0a4
David Perez <david.perez@trade.ec>
2023-10-17 21:39:28 -0500

    Add functionality to can add transactions to list of transactions using the form

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
-import Style from "./Listado.module.css"
-import { Items } from './Items';
+import Style from "./Listado.module.css";
+import PropTypes from "prop-types";
 
-const Listado = ({Listas, setListas, setEdit}) => {
+const Listado = (props) => {
+  const { transactionList } = props;
 
-  const eliminar = ({id}) => {
-    setListas(Listas.filter(lista=>lista.id!==id));
-  };
-
+  // const eliminar = ({ id }) => {
+  //   setTransactionList(Listas.filter((lista) => lista.id !== id));
+  // };
 
   return (
     <div className={Style.Listado}>
       <h1>Listado de movimientos</h1>
       <input type="search" />
-      <input type="radio" value='Todos' name='todos'></input>
+      <input type="radio" value="Todos" name="todos"></input>
       <label htmlFor="todos">Todos</label>
-      <input type="radio" value='Ingresos' name='ingresos'></input>
+      <input type="radio" value="Ingresos" name="ingresos"></input>
       <label htmlFor="ingresos">Ingresos</label>
-      <input type="radio" value='Gastos' name='gastos'></input>
+      <input type="radio" value="Gastos" name="gastos"></input>
       <label htmlFor="gastos">Gastos</label>
-      <br/>
+      <br />
       <div>
-        {Listas.map((lista) => (
-          <Items 
-          key={lista.id} 
-          lista={lista}
-          eliminar = {eliminar}
-          setEdit = {setEdit}
-          />
+        {transactionList.map((transaction, index) => (
+          <li key={`transaction${index}`}>
+            <div>
+              <button onClick={() => {}}>❌</button>
+              <button onClick={() => {}}>✎</button>
+            </div>
+            <div>
+              {transaction.title}
+              {transaction.amount}
+            </div>
+          </li>
         ))}
       </div>
-      
     </div>
   );
+};
+
+Listado.propTypes = {
+  transactionList: PropTypes.array,
+  setTransactionList: PropTypes.func,
 };
 
 export default Listado;
